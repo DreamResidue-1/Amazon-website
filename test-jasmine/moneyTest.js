48ee95a0fe44c7119edc288e335523e52a4da071
jamal <you@example.com>
2026-03-21 16:01:26 -0900

    Remove defualt tests and add money test

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,13 @@
+import { formatCurrency } from "../scripts/util/money.js";
+
+describe('Test suite: foramtCurrency', () =>{
+  it ('converts price cents to dollars ', ()=>{
+    expect(formatCurrency(200)).toEqual('$2.00')
+  })
+  it('works with 0', ()=>{
+    expect(formatCurrency(0)).toEqual('$0.00')
+  })
+  it('rounds up to nearest cents', () =>{
+    expect(formatCurrency(200.5)).toEqual('$2.01');
+  })
+})
